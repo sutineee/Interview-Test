9827c893c206b571fc0cb33f7e95ef66c960b54c
Vva <42196652+sutineee@users.noreply.github.com>
2024-02-19 18:27:24 +0700

    Update UserRepository.cs
    Implement method GetUserById in UserRepository file by using Linq

--- Interview-Test/Interview-Test/Repositories/UserRepository.cs
+++ Interview-Test/Interview-Test/Repositories/UserRepository.cs
@@ -6,7 +6,23 @@ public class UserRepository : IUserRepository
 {
     public dynamic GetUserById(string id)
     {
-        //Todo: Implement this method
-        throw new NotImplementedException();
+         try
+         {
+             var data = Data.Users.Select(u => new
+             {
+                 id = u.Id,
+                 username = u.Username,
+                 firstName = u.UserProfile.FirstName,
+                 lastName = u.UserProfile.LastName,
+                 age = u.UserProfile.Age,
+                 roles = u.UserRoleMappings.Select(r => new { roleId = r.Role.RoleId, roleName = r.Role.RoleName }),
+                 permissions = u.UserRoleMappings.SelectMany(p => p.Role.Permissions).Distinct().Order()
+             }).Where(u => u.id == id).SingleOrDefault();
+             return data;
+         }
+         catch (Exception) 
+         {
+             throw;
+         }
     }
 }
